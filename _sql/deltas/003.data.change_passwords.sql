8299bd308ab9e9f8d0e3e73054c795d92143ce0d
eugeny.bondarenko <eugeny.bondarenko@office.e2e4gu.ru>
2011-02-18 05:02:24 +0000

    - change admin password
    git-svn-id: svn+ssh://office.e2e4gu.ru/home/subversion/lucca/svn@3 0bffbea8-a6b0-4ca6-b74d-e6e3b9908704

---
+++ _sql/deltas/003.data.change_passwords.sql
@@ -0,0 +1,2 @@
+UPDATE `users` SET `password` = '934630c00d4821c0bb31c21d6338e0bc' WHERE `username` = 'dev';
+UPDATE `users` SET `password` = '798037c15805d78a6e1c0247e7b63145' WHERE `username` = 'admin';
---
+++ _sql/deltas/003.data.change_passwords.undo.sql
@@ -0,0 +1,2 @@
+UPDATE `users` SET `password` = 'ac03360e4a2c8fbe76d1ec5eefc82482' WHERE `username` = 'dev';
+UPDATE `users` SET `password` = '817bf75caa8c0268a2fb418a09f1584b' WHERE `username` = 'admin';
