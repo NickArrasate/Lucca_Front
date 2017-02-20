afad9d35ef1f5e9af3687966c6d6271b9de8a2a4
Nick <narrasate22@hotmail.com>
2017-02-19 17:14:27 -0800

    update images and layout of homepage per susan

---
+++ js/navbar.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  //Handles Collapse state of dropdown menus if screen size changes
+  $(window).resize(function() {
+    var x = document.getElementsByClassName('collapse')
+      if ($(window).width() > 768) {
+          $(x).removeClass('in');
+      } else {
+        return
+      }
+  }).resize(); // trigger resize event initially
+})
