05ebd50c879b3e988a046f88c5a26209f5a78257
Nick <narrasate22@hotmail.com>
2017-04-09 23:47:53 -0700

    add ipad-smooth script to each page and a js file to allow smooth scrolling on mobile.

--- js/ipad-smooth.js
+++ js/ipad-smooth.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+  console.log('hi');
   var deviceAgent = navigator.userAgent.toLowerCase();
+  console.log(deviceAgent, 'hello');
   var agentID = deviceAgent.match(/(iPad|iPhone|iPod)/i);
   if(agentID){
     $('body').css('-webkit-overflow-scrolling', 'touch');
