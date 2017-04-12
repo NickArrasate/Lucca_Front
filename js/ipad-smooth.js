09015c2b7cbf206d3fc56069e9f503e0780ca51d
Nick <narrasate22@hotmail.com>
2017-04-12 13:38:26 -0700

    add image to about page with caption and associated styles.

--- js/ipad-smooth.js
+++ js/ipad-smooth.js
@@ -1,7 +1,5 @@
 $(document).ready(function(){
-  console.log('hi');
   var deviceAgent = navigator.userAgent.toLowerCase();
-  console.log(deviceAgent, 'hello');
   var agentID = deviceAgent.match(/(iPad|iPhone|iPod)/i);
   if(agentID){
     $('body').css('-webkit-overflow-scrolling', 'touch');
